5240e687c8a89e9adcf3401d67816aa9d378a0f8
avilego <57768269+avilego@users.noreply.github.com>
2020-04-09 23:22:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.querySelector("#hero").addEventListener("click", function () {
+//   document.getElementById("texto1").style.display = "block";
+//   document.getElementById("texto1").style.visibility = "visible";
+// });
+function change() {
+  document.getElementById("texto1").style.color = "transparent";
+  console.log("hola");
+}
